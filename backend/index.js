4d03f99f570537dd3b83bc4175833e6df991e020
faizan alam <alamf6023@gmail.com>
2023-09-26 21:00:25 +0530

    write mongodb connector code

--- backend/index.js
+++ backend/index.js
@@ -1,1 +1,8 @@
-console.log("hello backend")
+const express = require('express')
+const app = express();
+const mongoose = require('mongoose')
+//username : faizanalam
+//password : g64zIyZetMbGlzND
+//connector : mongodb+srv://faizanalam:<password>@elitiancollab.6lq5vd9.mongodb.net/?retryWrites=true&w=majority
+
+// mongodb+srv://faizanalam:g64zIyZetMbGlzND@elitiancollab.6lq5vd9.mongodb.net/elitian_collab?retryWrites=true&w=majority
