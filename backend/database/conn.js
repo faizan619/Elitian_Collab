3fdb37b094130800f91667cd356677bf4738eac3
faizan alam <alamf6023@gmail.com>
2023-09-27 18:46:35 +0530

    database logic created

--- backend/database/conn.js
+++ backend/database/conn.js
@@ -1,3 +1,8 @@
 const mongoose = require('mongoose')
 const DB = process.env.DATABASE;
-mongoose.connect();
+mongoose.connect(DB).then(()=>{
+    console.log("DataBase Connected")
+})
+.catch((err)=> {
+    console.log(`There is an error : ${err}`)
+})
