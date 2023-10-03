37c84e97bfd4019b381237b9bb4a0e069b9decf1
faizan alam <alamf6023@gmail.com>
2023-10-03 16:53:57 +0530

    added rgb color generator

--- frontend/src/bgChanger/App.jsx
+++ frontend/src/bgChanger/App.jsx
@@ -2,12 +2,21 @@ import React, { useState } from 'react'
 
 function App() {
     const [color,setcolor] = useState('');
-    const [design,setdesign] = useState('');
+
+    const randomColor = ()=>{
+        let randomVal1 = Math.floor(Math.random()*256);
+        let randomVal2 = Math.floor(Math.random()*256);
+        let randomVal3 = Math.floor(Math.random()*256);
+        setcolor(`rgb(${randomVal1},${randomVal2},${randomVal3})`)
+    }
   return (
     <>
         <div className="bgcontainer" style={{backgroundColor:`${color}`}}>
             <div className="bglayout">
-            <input type="text" placeholder='Enter background Color Name...' autoFocus onChange={e => setcolor(e.target.value)} />
+            {/* <input type="text" placeholder='Enter background Color Name...' autoFocus onChange={e => setcolor(e.target.value)} /> */}
+            <button onClick={randomColor}>Generate RGB color</button>
+            <p>color code : {color}</p>
+            
             </div>
         </div>
     </>
